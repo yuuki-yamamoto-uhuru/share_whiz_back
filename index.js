19295187e5b3658eeed5fdafa24d0aebc0be3d24
yuuki-yamamoto-uhuru <yamamoto.yuki@uhuru.jp>
2025-05-09 12:39:56 +0900

    add: app.use(cors());

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const postAPI = require('./api/post')
 const sampleAPI = require('./api/sample')
@@ -6,11 +7,14 @@ const sampleAPI = require('./api/sample')
 const app = express()
 const PORT = 3000
 
+// 全てのオリジンからのリクエストを許可する。
+app.use(cors());
+
 // ExpressでJsonを取得 https://zenn.dev/urinco/articles/e3932af54c020e
 app.use(express.json())
 
 app.use('/api/hello', sampleAPI); // サンプル
-app.use('/api/posts', postAPI);
+app.use('/api/post', postAPI);
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
